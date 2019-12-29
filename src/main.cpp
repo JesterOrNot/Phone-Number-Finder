30ef884207a9d1d74324755f2dd6b450ecfd54d8
Sean Hellum <seanhellum45@gmail.com>
2019-12-29 20:26:05 +0000

    initialize code skele

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+string readFileToString(string filename) {
+  // WIP
+}
+
+vector<string> findPhoneNumbers(string contents) {
+  // WIP
+}
+
+int main() {
+  // WIP
+  return 0;
+}
